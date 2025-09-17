f482bcf73c1cfedc07fca046cc92e89033accdb8
l0450 <wiktorkumor87@gmail.com>
2025-09-17 13:50:30 +0200

    Chapter 3 completed.

---
+++ Chapter3.py
@@ -0,0 +1,155 @@
+# 11. Coin Flip
+
+import random
+
+num = random.randint(0, 1)   # Generates a random number that's either 0 or 1
+
+if num > 0.5:
+  print('Heads')
+else:
+  print('Tails')
+
+# 12 Grades
+
+grade = 53
+
+if grade >= 55:
+  print("You passed.")
+else:
+  print("You failed.")  
+
+# 13. pH Levels
+
+ph = 10
+
+if ph > 7:
+  print("Basic")
+elif ph < 7:
+  print("Acidic")
+else:
+  print("Neutral")
+
+# 14. Magic 8 Ball
+
+question = input('Question:      ')
+
+random_number = random.randint(1, 9)
+
+if random_number == 1:
+  answer = 'Yes - definitely'
+elif random_number == 2:
+  answer = 'It is decidedly so'
+elif random_number == 3:
+  answer = 'Without a doubt'
+elif random_number == 4:
+  answer = 'Reply hazy, try again'
+elif random_number == 5:
+  answer = 'Ask again later'
+elif random_number == 6:
+  answer = 'Better not tell you now'
+elif random_number == 7:
+  answer = 'My sources say no'
+elif random_number == 8:
+  answer = 'Outlook not so good'
+elif random_number == 9:
+  answer = 'Very doubtful'
+else:
+  answer = 'Error'
+  
+print('Magic 8 Ball:  ' + answer)
+
+# 15. The Cyclone
+
+height = 160
+credits = 15
+
+if height >= 137 and credits >= 10:
+  print("Enjoy the ride!")
+elif height < 137 and credits >= 10:
+  print("You are not tall enought to ride.")
+elif height >= 137 and credits <= 10:
+  print("You don't have enough credits.")
+else:
+  print("You are not meeting the requirements.")
+
+# 16. Sorting Hat
+
+gryffindor = 0
+hufflepuff = 0
+ravenclaw = 0
+slytherin = 0
+
+print('===============')
+print('The Sorting Hat')
+print('===============')
+
+print('Q1) Do you like Dawn or Dusk?')
+
+print('  1) Dawn')
+print('  2) Dusk')
+
+answer = int(input('Enter answer (1-2): '))
+
+if answer == 1:
+  gryffindor += 1
+  ravenclaw += 1
+elif answer == 2:
+  hufflepuff += 1
+  slytherin +=1
+else:
+  print('Wrong input.')
+
+print("\nQ2) When I'm dead, I want people to remember me as:")
+
+print('  1) The Good')
+print('  2) The Great')
+print('  3) The Wise')
+print('  4) The Bold')
+
+answer = int(input('Enter your answer (1-4): '))
+
+if answer == 1:
+  hufflepuff += 2
+elif answer == 2:
+  slytherin += 2
+elif answer == 3:
+  ravenclaw += 2
+elif answer == 4:
+  gryffindor += 2
+else:
+  print('Wrong input.')
+
+print('\nQ3) Which kind of instrument most pleases your ear?')
+
+print('  1) The violin')
+print('  2) The trumpet')
+print('  3) The piano')
+print('  4) The drum')
+
+answer = int(input('Enter your answer (1-4): '))
+
+if answer == 1:
+  slytherin += 4
+elif answer == 2:
+  hufflepuff += 4
+elif answer == 3:
+  ravenclaw +=4
+elif answer == 4:
+  gryffindor += 4
+else:
+  print('Wrong input.')
+  
+print("Gryffindor: ", gryffindor)
+print("Ravenclaw: ", ravenclaw)
+print("Hufflepuff: ", hufflepuff)
+print("Slytherin: ", slytherin)
+
+most_points = max(gryffindor, ravenclaw, hufflepuff, slytherin) 
+if gryffindor == most_points:
+  print('🦁 Gryffindor!')
+elif ravenclaw == most_points:
+  print('🦅 Ravenclaw!')
+elif hufflepuff == most_points:
+  print('🦡 Hufflepuff!')
+else:
+  print('🐍 Slytherin!')
